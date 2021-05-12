4a50afe9dcc7040a392e3de2b5ad7ab320497939
kiokoi <kiowinny@gmail.com>
2021-05-11 23:32:51 -0500

    modificacion de camara

--- app/src/main/java/com/ramirez/soto/dev/mapa/MapsActivity.java
+++ app/src/main/java/com/ramirez/soto/dev/mapa/MapsActivity.java
@@ -8,6 +8,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -40,7 +41,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.addMarker(new MarkerOptions().position(sydney).title("P. sherman"));
+
+        CameraPosition camera =new CameraPosition.Builder()
+                .target(sydney)
+                .zoom(18)
+                .bearing(0)
+                .tilt(30)
+                .build();
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(camera));
+
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
